86fd401d0fc35abb7722c0a4f18e92f96ef59e7c
Alicia Pan <alicia.pan@uwaterloo.ca>
2024-09-19 12:43:44 -0400

    Set up idea for selecting most recently added cake component as the selected item

--- overcaked-front-end/src/pages/MainPage/MainPage.jsx
+++ overcaked-front-end/src/pages/MainPage/MainPage.jsx
@@ -56,6 +56,7 @@ function MainPage() {
     function addCakeLayer() {
         if (cakelayers.length < 3 && icing === "") {
             setCakelayers([...cakelayers, "vanilla"]);
+            // setSelectedItem(cakelayers[cakelayers.length-1]);
         }
 
         else {
@@ -67,6 +68,7 @@ function MainPage() {
     function addIcingLayer() {
         if (icing.length < 1 && cakelayers.length > 0) {
             setIcing("vanilla");
+            // setSelectedItem(icing);
         }
 
         else {
