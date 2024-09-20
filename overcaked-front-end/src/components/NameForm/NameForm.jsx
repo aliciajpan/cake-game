d17c510a538153dc87038dceb19edb10f03d6f7a
Alicia Pan <alicia.pan@uwaterloo.ca>
2024-09-19 21:50:28 -0400

    Experiment with several form methods in React (controlled vs uncontrolled, state vs ref vs raw onSubmit)

--- overcaked-front-end/src/components/NameForm/NameForm.jsx
+++ overcaked-front-end/src/components/NameForm/NameForm.jsx
@@ -1,47 +1,81 @@
 import { useNavigate } from "react-router-dom";
+import { useState, useEffect, useRef } from "react";
 import Button from "../Button/Button";
 import "./NameForm.scss";
 
 function NameForm() {
     const navigate = useNavigate();
 
-    function submitName(event) {
-        event.preventDefault();
-        const saveThisName = event.target.name.value.trim();
+    // this was for if onChange and value instead of defaultValue
+    // const [formInput, setFormInput] = useState("");
 
-        // TODO: DEAL W LOGIC HERE
-        if (localStorage.getItem("overcakedSavedName")) {
-            navigate("/play");
-            localStorage.setItem("overcakedSavedName", localStorage.getItem("overcakedSavedName"));
-        }
+    // useEffect(() => {
+    //     const storedName = localStorage.getItem("overcakedSavedName");
+    //     if (storedName) {
+    //         setFormInput(storedName);
+    //     }
+    // }, []);
 
-        if (!saveThisName && !localStorage.getItem("overcakedSavedName")) {
-            const nameInfo = confirm("Names are used for the scoreboard. Do you want to remain anonymous?");
-            if (nameInfo) {
-                localStorage.setItem("overcakedSavedName", saveThisName);
-                navigate("/play");
-            }
-        }
+    // function onChange (event) {
+    //     let saveThis = event.target.name.value;
+    //     setFormInput(saveThis);
+    //     localStorage.setItem("overcakedSavedName", saveThis)
+    // }
 
-        else {
-            localStorage.setItem("overcakedSavedName", saveThisName);
-            navigate("/play");
+    const nameInputRef = useRef(null);
+
+    useEffect(() => {
+        const storedName = localStorage.getItem("overcakedSavedName");
+        if (storedName) {
+            nameInputRef.current.value = storedName;
         }
+    }, [])
+
+    function handleSubmit() {
+        localStorage.setItem("overcakedSavedName", nameInputRef.current.value);
+        navigate("/play");
     }
+
+    // function submitName(event) {
+    //     event.preventDefault();
+    //     const saveThisName = event.target.name.value.trim();
+
+    //     // TODO: DEAL W LOGIC HERE
+    //     if (localStorage.getItem("overcakedSavedName")) {
+    //         navigate("/play");
+    //         localStorage.setItem("overcakedSavedName", localStorage.getItem("overcakedSavedName"));
+    //     }
+
+    //     if (!saveThisName && !localStorage.getItem("overcakedSavedName")) {
+    //         const nameInfo = confirm("Names are used for the scoreboard. Do you want to remain anonymous?");
+    //         if (nameInfo) {
+    //             localStorage.setItem("overcakedSavedName", saveThisName);
+    //             navigate("/play");
+    //         }
+    //     }
+
+    //     else {
+    //         localStorage.setItem("overcakedSavedName", saveThisName);
+    //         navigate("/play");
+    //     }
+    // }
 
     return (
         <>
-        <form className="nameform" onSubmit={submitName}>
-            <input 
+        <form className="nameform" /*onSubmit={submitName}*/>
+            <input ref={nameInputRef}
                 className="nameform__input" 
                 name="name" 
                 id="name" 
-                type="text" 
-                placeholder={localStorage.getItem("overcakedSavedName") || "what's your name, chef?"}
+                type="text"
+                //defaultValue={formInput} 
+                // value={formInput}
+                placeholder={/*localStorage.getItem("overcakedSavedName") || */"what's your name, chef?"}
+                // onChange={onChange}
             ></input>
             {/* <Button text="Submit" sizing="form" color="brown"/> */}
                 {/* form sizing does not actually exist */}
-            <Button text="play" sizing="big" color="pink"/>
+            <Button text="play" sizing="big" color="pink" onClick={handleSubmit}/>
         </form>
         {/*<NavLink to="/play"><Button text="play" sizing="big" color="pink"/></NavLink>*/}
         </>
