7c36754f6109b9db03528556f15cf81ff9b4bdec
Alicia Pan <alicia.pan@uwaterloo.ca>
2024-09-19 22:36:36 -0400

    Polish name input logic, allowing users to remain anonymous and prefilling the input field with what is saved in localStorage

--- overcaked-front-end/src/components/NameForm/NameForm.jsx
+++ overcaked-front-end/src/components/NameForm/NameForm.jsx
@@ -1,83 +1,48 @@
 import { useNavigate } from "react-router-dom";
-import { useState, useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import Button from "../Button/Button";
 import "./NameForm.scss";
 
 function NameForm() {
     const navigate = useNavigate();
+    const nameInputRef = useRef(null); // handles input default value
 
-    // this was for if onChange and value instead of defaultValue
-    // const [formInput, setFormInput] = useState("");
-
-    // useEffect(() => {
-    //     const storedName = localStorage.getItem("overcakedSavedName");
-    //     if (storedName) {
-    //         setFormInput(storedName);
-    //     }
-    // }, []);
-
-    // function onChange (event) {
-    //     let saveThis = event.target.name.value;
-    //     setFormInput(saveThis);
-    //     localStorage.setItem("overcakedSavedName", saveThis)
-    // }
-
-    const nameInputRef = useRef(null);
-
-    useEffect(() => {
+    useEffect(() => { // set preloaded value once on mount
         const storedName = localStorage.getItem("overcakedSavedName");
         if (storedName) {
             nameInputRef.current.value = storedName;
         }
     }, [])
 
-    function handleSubmit() {
-        localStorage.setItem("overcakedSavedName", nameInputRef.current.value);
+    function handleSubmit(event) {
+        event.preventDefault();
+        const nameInputted = nameInputRef.current.value.trim();
+        
+        if (!nameInputted) {
+            const confirm = window.confirm("Names are used for the scoreboard. Click OK to remain anonymous");
+            
+            if (!confirm) {
+                return;
+            } 
+        }
+
+        localStorage.setItem("overcakedSavedName", nameInputted);
         navigate("/play");
     }
 
-    // function submitName(event) {
-    //     event.preventDefault();
-    //     const saveThisName = event.target.name.value.trim();
-
-    //     // TODO: DEAL W LOGIC HERE
-    //     if (localStorage.getItem("overcakedSavedName")) {
-    //         navigate("/play");
-    //         localStorage.setItem("overcakedSavedName", localStorage.getItem("overcakedSavedName"));
-    //     }
-
-    //     if (!saveThisName && !localStorage.getItem("overcakedSavedName")) {
-    //         const nameInfo = confirm("Names are used for the scoreboard. Do you want to remain anonymous?");
-    //         if (nameInfo) {
-    //             localStorage.setItem("overcakedSavedName", saveThisName);
-    //             navigate("/play");
-    //         }
-    //     }
-
-    //     else {
-    //         localStorage.setItem("overcakedSavedName", saveThisName);
-    //         navigate("/play");
-    //     }
-    // }
-
     return (
         <>
-        <form className="nameform" /*onSubmit={submitName}*/>
-            <input ref={nameInputRef}
-                className="nameform__input" 
-                name="name" 
-                id="name" 
-                type="text"
-                //defaultValue={formInput} 
-                // value={formInput}
-                placeholder={/*localStorage.getItem("overcakedSavedName") || */"what's your name, chef?"}
-                // onChange={onChange}
-            ></input>
-            {/* <Button text="Submit" sizing="form" color="brown"/> */}
-                {/* form sizing does not actually exist */}
-            <Button text="play" sizing="big" color="pink" onClick={handleSubmit}/>
-        </form>
-        {/*<NavLink to="/play"><Button text="play" sizing="big" color="pink"/></NavLink>*/}
+            <form className="nameform">
+                <input 
+                    ref={nameInputRef}
+                    className="nameform__input" 
+                    name="name" 
+                    id="name" 
+                    type="text"
+                    placeholder={"what's your name, chef?"}
+                ></input>
+                <Button text="play" sizing="big" color="pink" onClick={handleSubmit}/>
+            </form>
         </>
     );
 }
