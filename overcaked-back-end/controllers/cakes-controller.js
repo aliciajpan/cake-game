6d018bf87acd4a0816dbbbcd184497376ef2b933
Alicia Pan <alicia.pan@uwaterloo.ca>
2024-09-22 21:49:45 -0400

    Populate cakes.json with unchanging sequence of cake orders

--- overcaked-back-end/controllers/cakes-controller.js
+++ overcaked-back-end/controllers/cakes-controller.js
@@ -4,42 +4,184 @@ import generateNum from '../utils/rng.js';
 
 function generateCakes (_req, res) {
     try {
-        const cakes = [];
-
-        for (let i=0; i<20; i++) {
-            const generatedNum = generateNum(3);
-            const cakeLayers = [];
-            const icingFlavour = flavours[generateNum(3)-1];
-            let pointsForSuccess = 0;
-
-            for (let i=0; i<generatedNum; i++) {
-                cakeLayers.push(flavours[generateNum(3)-1]);
+        const cakes = [
+            {
+              "id": 1,
+              "layerCount": 1,
+              "layers": ["strawberry"],
+              "icing": "strawberry",
+              "points": 200
+            },
+            {
+              "id": 2,
+              "layerCount": 2,
+              "layers": ["chocolate", "vanilla"],
+              "icing": "chocolate",
+              "points": 300
+            },
+            {
+              "id": 3,
+              "layerCount": 1,
+              "layers": ["chocolate"],
+              "icing": "chocolate",
+              "points": 200
+            },
+            {
+              "id": 4,
+              "layerCount": 3,
+              "layers": ["vanilla", "strawberry", "chocolate"],
+              "icing": "chocolate",
+              "points": 450
+            },
+            {
+              "id": 5,
+              "layerCount": 1,
+              "layers": ["strawberry"],
+              "icing": "strawberry",
+              "points": 200
+            },
+            {
+              "id": 6,
+              "layerCount": 2,
+              "layers": ["strawberry", "strawberry"],
+              "icing": "vanilla",
+              "points": 300
+            },
+            {
+              "id": 7,
+              "layerCount": 2,
+              "layers": ["vanilla", "strawberry"],
+              "icing": "chocolate",
+              "points": 300
+            },
+            {
+              "id": 8,
+              "layerCount": 2,
+              "layers": ["chocolate", "vanilla"],
+              "icing": "vanilla",
+              "points": 250
+            },
+            {
+              "id": 9,
+              "layerCount": 1,
+              "layers": ["strawberry"],
+              "icing": "chocolate",
+              "points": 200
+            },
+            {
+              "id": 10,
+              "layerCount": 2,
+              "layers": ["vanilla", "strawberry"],
+              "icing": "chocolate",
+              "points": 300
+            },
+            {
+              "id": 11,
+              "layerCount": 2,
+              "layers": ["vanilla", "vanilla"],
+              "icing": "chocolate",
+              "points": 250
+            },
+            {
+              "id": 12,
+              "layerCount": 2,
+              "layers": ["vanilla", "strawberry"],
+              "icing": "strawberry",
+              "points": 300
+            },
+            {
+              "id": 13,
+              "layerCount": 1,
+              "layers": ["chocolate"],
+              "icing": "chocolate",
+              "points": 200
+            },
+            {
+              "id": 14,
+              "layerCount": 3,
+              "layers": ["chocolate", "chocolate", "chocolate"],
+              "icing": "strawberry",
+              "points": 500
+            },
+            {
+              "id": 15,
+              "layerCount": 2,
+              "layers": ["chocolate", "chocolate"],
+              "icing": "chocolate",
+              "points": 350
+            },
+            {
+              "id": 16,
+              "layerCount": 1,
+              "layers": ["vanilla"],
+              "icing": "chocolate",
+              "points": 150
+            },
+            {
+              "id": 17,
+              "layerCount": 2,
+              "layers": ["strawberry", "vanilla"],
+              "icing": "chocolate",
+              "points": 300
+            },
+            {
+              "id": 18,
+              "layerCount": 2,
+              "layers": ["vanilla", "chocolate"],
+              "icing": "vanilla",
+              "points": 250
+            },
+            {
+              "id": 19,
+              "layerCount": 2,
+              "layers": ["vanilla", "vanilla"],
+              "icing": "chocolate",
+              "points": 250
+            },
+            {
+              "id": 20,
+              "layerCount": 2,
+              "layers": ["vanilla", "strawberry"],
+              "icing": "chocolate",
+              "points": 300
             }
+          ]
+        // const cakes = [];
 
-            for (let i=0; i<cakeLayers.length; i++) {
-                if (cakeLayers[i] === "vanilla") {
-                    pointsForSuccess += 100;
-                }
+        // for (let i=0; i<20; i++) {
+        //     const generatedNum = generateNum(3);
+        //     const cakeLayers = [];
+        //     const icingFlavour = flavours[generateNum(3)-1];
+        //     let pointsForSuccess = 0;
 
-                else {
-                    pointsForSuccess += 150;
-                }
-            }
+        //     for (let i=0; i<generatedNum; i++) {
+        //         cakeLayers.push(flavours[generateNum(3)-1]);
+        //     }
 
-            if (icingFlavour !== "vanilla") {
-                pointsForSuccess += 50;
-            }
+        //     for (let i=0; i<cakeLayers.length; i++) {
+        //         if (cakeLayers[i] === "vanilla") {
+        //             pointsForSuccess += 100;
+        //         }
 
-            cakes.push(
-                {
-                    id: i+1,
-                    layerCount: generatedNum,
-                    layers: cakeLayers,
-                    icing: icingFlavour,
-                    points: pointsForSuccess
-                }
-            )
-        }
+        //         else {
+        //             pointsForSuccess += 150;
+        //         }
+        //     }
+
+        //     if (icingFlavour !== "vanilla") {
+        //         pointsForSuccess += 50;
+        //     }
+
+        //     cakes.push(
+        //         {
+        //             id: i+1,
+        //             layerCount: generatedNum,
+        //             layers: cakeLayers,
+        //             icing: icingFlavour,
+        //             points: pointsForSuccess
+        //         }
+        //     )
+        // }
 
         writeFile("cakes.json", cakes);
         res.json(cakes);
